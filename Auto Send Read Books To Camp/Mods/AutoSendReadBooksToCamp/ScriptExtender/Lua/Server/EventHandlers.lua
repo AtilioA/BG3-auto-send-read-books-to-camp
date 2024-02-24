9d9caf6719539b1ae943a0786a74e79430a00fd4
Atílio <atiliodadalto@protonmail.com>
2024-02-23 21:29:12 -0300

    fix: don't try to load 'Mark book as read' so soon

--- Auto Send Read Books To Camp/Mods/AutoSendReadBooksToCamp/ScriptExtender/Lua/Server/EventHandlers.lua
+++ Auto Send Read Books To Camp/Mods/AutoSendReadBooksToCamp/ScriptExtender/Lua/Server/EventHandlers.lua
@@ -2,6 +2,8 @@ Ext.Require("Server/Helpers/Book.lua")
 
 EHandlers = {}
 
+FALLENS_MARK_BOOK_AS_READ = "c72d9f6a-a6e4-48b1-98c0-0ecdc7c31cf7"
+
 function EHandlers.OnTeleportedToCamp(character)
   if Osi.IsInPartyWith(character, Osi.GetHostCharacter()) == 1 then
     Utils.DebugPrint(2, "Sending existing Book to chest from " .. character)
@@ -9,9 +11,30 @@ function EHandlers.OnTeleportedToCamp(character)
   end
 end
 
+function EHandlers.OnTimerFinished(timer)
+  if timer == "FMBR_LoadTimer" then
+    EHandlers.TryToLoadFallenVars()
+  end
+end
+
 function EHandlers.OnGameBookInterfaceClosed(item, character)
   Utils.DebugPrint(2, "GameBookInterfaceClosed event triggered.")
   BookDelivery.SendOwnedBookToChest(character, item)
+end
+
+function EHandlers.TryToLoadFallenVars()
+  if (Ext.Mod.IsModLoaded(FALLENS_MARK_BOOK_AS_READ)) then
+    FMBRVars = Ext.Vars.GetModVariables(FALLENS_MARK_BOOK_AS_READ)
+    Utils.DebugPrint(2, "MarkBookAsRead has been loaded successfully.")
+  else
+    _D("[Auto Send Read Books To Camp][ERROR]: MarkBookAsRead mod is not loaded, mayhem will ensue.")
+-- Probably unnecessary, but just in case
+    Osi.TimerLaunch("FMBR_LoadTimer", 5000)
+  end
+end
+
+function EHandlers.OnLevelGameplayStarted()
+  EHandlers.TryToLoadFallenVars()
 end
 
 return EHandlers
--- Auto Send Read Books To Camp/Mods/AutoSendReadBooksToCamp/ScriptExtender/Lua/Server/SubscribedEvents.lua
+++ Auto Send Read Books To Camp/Mods/AutoSendReadBooksToCamp/ScriptExtender/Lua/Server/SubscribedEvents.lua
@@ -2,6 +2,9 @@ local function SubscribeToEvents()
   if JsonConfig.GENERAL.enabled == true then
     Utils.DebugPrint(2, "Subscribing to events with JSON config: " .. Ext.Json.Stringify(JsonConfig, { Beautify = true }))
 
+    Ext.Osiris.RegisterListener("LevelGameplayStarted", 2, "before", EHandlers.OnLevelGameplayStarted)
+    Ext.Osiris.RegisterListener("TimerFinished", 1, "after", EHandlers.OnTimerFinished)
+
     Ext.Osiris.RegisterListener("TeleportedToCamp", 1, "before", EHandlers.OnTeleportedToCamp)
 
     if JsonConfig.FEATURES.instantly == true then
