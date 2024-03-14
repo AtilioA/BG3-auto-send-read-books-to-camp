b85d0d8d62469a1e2854cbe3f9632b1e8df6e427
AtilioA <atiliogpg@protonmail.com>
2024-03-14 14:57:06 -0300

    refactor: delete Book.lua, Ware.lua, and Utils.lua files, update Config.lua file

---
+++ Auto Send Read Books To Camp/Mods/AutoSendReadBooksToCamp/ScriptExtender/Lua/Server/Helpers/Config.lua
@@ -0,0 +1,38 @@
+Config = Helpers.Config:New({
+    folderName = "AutoSendReadBooksToCamp",
+    configFilePath = "auto_send_read_books_to_camp_config.json",
+    defaultConfig = {
+        GENERAL = {
+            enabled = true, -- Toggle the mod on/off
+        },
+        FEATURES = {
+            instantly = {
+                enabled = false,     -- Move books right after reading them (instead of waiting for teleport to camp)
+                mark_as_ware_instead = {
+                    enabled = false, -- Mark books as wares after reading them. Will be ignored if `instantly.enabled` is set to true
+                    -- only_duplicates = true, -- Only mark duplicate books as wares
+                }
+            },
+            ignore = {
+                quest = true, -- Ignore quest books
+                wares = true, -- Ignore books marked as wares
+            }
+        },
+        DEBUG = {
+            level = 0 -- 0 = no debug, 1 = minimal, 2 = verbose logs
+        }
+    }
+})
+
+Config:UpdateCurrentConfig()
+
+Config:AddConfigReloadedCallback(function(configInstance)
+    ASRBTCPrinter.DebugLevel = configInstance:GetCurrentDebugLevel()
+    ASRBTCPrint(0, "Config reloaded: " .. Ext.Json.Stringify(configInstance:getCfg(), { Beautify = true }))
+end)
+Config:RegisterReloadConfigCommand("asrbtc")
+
+
+Config = {}
+
+Config.configFilePath = ".json"
